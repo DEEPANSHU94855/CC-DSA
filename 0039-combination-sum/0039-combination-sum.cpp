d48a604590f97a3a4fc990b9ecc55c290064c741
DEEPANSHU94855 <deepanshu98405@gmail.com>
2026-09-07 15:35:50 +0530

    Time: 3 ms (51.4%), Space: 14.2 MB (58.11%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void solve(int start, vector<int>& candidates, int target,vector<int>& current, vector<vector<int>>& ans) {
+        if(target==0) {
+            ans.push_back(current);
+            return;
+        }
+        for(int i=start;i<candidates.size();i++){
+            if(candidates[i]>target){
+                break;
+            }
+            current.push_back(candidates[i]);
+            solve(i,candidates,target -candidates[i],current,ans);
+            current.pop_back();
+        }
+    }
+    vector<vector<int>>combinationSum(vector<int>& candidates,int target){
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>>ans;
+        vector<int>current;
+        solve(0,candidates,target,current,ans);
+        return ans;
+    }
+};
