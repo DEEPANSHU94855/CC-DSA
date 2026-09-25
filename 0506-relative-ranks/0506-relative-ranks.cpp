31dd8b89cbedd18c926f96b0b8bca5b29804c77c
DEEPANSHU94855 <deepanshu98405@gmail.com>
2026-09-25 15:42:07 +0530

    Time: 3 ms (79.62%), Space: 15.4 MB (86.66%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,25 +1,53 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        vector<pair<int,int>> v;
+
         int n=score.size();
         vector<string> ans(n);
+        priority_queue<pair<int,int>> pq;
+
         for(int i=0;i<n;i++){
-            v.push_back({score[i],i});
+            pq.push({score[i],i});
         }
-        sort(v.begin(),v.end(),greater<>());
-        for(int rank=0;rank<n;rank++){
-            int index=v[rank].second;
-            if(rank==0){
+        int rank=1;
+        int index;
+        for(rank=1;rank<=n;rank++){
+            index=pq.top().second;
+            pq.pop();
+            if(rank==1){
                 ans[index]="Gold Medal";
-            }else if(rank==1){
-                ans[index]="Silver Medal";
             }else if(rank==2){
+                ans[index]="Silver Medal";
+            }else if(rank==3){
                 ans[index]="Bronze Medal";
             }else{
-                ans[index]=to_string(rank+1);
+                ans[index]=to_string(rank);
             }
         }
         return ans;
+
+
+        // using vector of pair approach adn time complexity is O(n log n) and space is O(n).
+
+        // vector<pair<int,int>> v;
+        // int n=score.size();
+        // vector<string> ans(n);
+        // for(int i=0;i<n;i++){
+        //     v.push_back({score[i],i});
+        // }
+        // sort(v.begin(),v.end(),greater<>());
+        // for(int rank=0;rank<n;rank++){
+        //     int index=v[rank].second;
+        //     if(rank==0){
+        //         ans[index]="Gold Medal";
+        //     }else if(rank==1){
+        //         ans[index]="Silver Medal";
+        //     }else if(rank==2){
+        //         ans[index]="Bronze Medal";
+        //     }else{
+        //         ans[index]=to_string(rank+1);
+        //     }
+        // }
+        // return ans;
     }
 };
